c397101f39ccc91edc3c70d2e7b34b8641b9939b
AlejandroPenagos84 <alejopenagos79@gmail.com>
2023-11-12 17:38:33 -0500

    Cambios a la interfaz y se añade LabelCell

--- RummiQ/src/Elements/ViewBoard.java
+++ RummiQ/src/Elements/ViewBoard.java
@@ -34,9 +34,9 @@ public class ViewBoard extends javax.swing.JFrame
     BoardLogic boardLogic;//Logica Del Tablero
 
     private JPanel panelsBoard[][];
-    private JLabel labelsBoard[][];
+    private LabelCell labelsBoard[][];
     private JPanel cardPanelsContainer[][];
-    private JLabel cardLabelsContainer[][];
+    private LabelCell cardLabelsContainer[][];
     private JPanel cardContainer;
 
     private final int widthBoard, heigthBoard, widthContainer, heigthContainer;
@@ -45,15 +45,15 @@ public class ViewBoard extends javax.swing.JFrame
 
     public ViewBoard() {
         this.panelsBoard = new JPanel[13][8];
-        this.labelsBoard = new JLabel[13][8];
+        this.labelsBoard = new LabelCell[13][8];
         this.cardPanelsContainer = new JPanel[9][5];
-        this.cardLabelsContainer = new JLabel[9][5];
+        this.cardLabelsContainer = new LabelCell[9][5];
 
         this.widthContainer = 625;
-        this.heigthContainer = 637;
+        this.heigthContainer = 437;
 
-        this.widthBoard = 900;
-        this.heigthBoard = 900;
+        this.widthBoard = 865;
+        this.heigthBoard = 700;
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -74,14 +74,23 @@ public class ViewBoard extends javax.swing.JFrame
         for (int rows = 0; rows < 13; ++rows) {
             for (int col = 0; col < 8; ++col) {
                 panelsBoard[rows][col] = new JPanel();//Crear Paneles
-                labelsBoard[rows][col] = new JLabel();//Crear Labels
+                labelsBoard[rows][col] = new LabelCell(rows, col);//Crear Labels
                 panelsBoard[rows][col].add(labelsBoard[rows][col]);//Añade el label dentro del panel creador
-                panelsBoard[rows][col].setSize((widthBoard - 35) / 13, (heigthBoard - 200) / 8);//Acomoda el tamaño del panel
+                panelsBoard[rows][col].setSize((widthBoard) / 13, (heigthBoard) / 8);//Acomoda el tamaño del panel
                 panelsBoard[rows][col].setLocation(auxW, auxH);//Acomoda al panel en la posición dada
-                auxH += (heigthBoard - 200) / 8;//Aumenta auxH para colocar el sigueinte label en la siguiente columna
+
+                //labelsBoard[rows][col].setPosICol(auxH+10);
+                //labelsBoard[rows][col].setPosIFilas(auxW+10);
+                
+                int auxA = auxW + (widthBoard) / 9;
+                auxH += (heigthBoard) / 8;//Aumenta auxH para colocar el sigueinte label en la siguiente columna
+                
+                //labelsBoard[rows][col].setPosFCol(auxH - 10);
+                //labelsBoard[rows][col].setPosFFilas(auxA-10);
+                
                 this.add(panelsBoard[rows][col]);//Añade el panel en el Frame
             }
-            auxW += (widthBoard - 35) / 13; // Aumenta AuxW para colocarlo posicionarse en la nueva Fila
+            auxW += (widthBoard) / 13; // Aumenta AuxW para colocarlo posicionarse en la nueva Fila
             auxH = 20; // Reinicia auxH para colocar toda la nueva fila desde la primera columna
         }
         paintBoard();//Pinta todo el tablero
@@ -98,18 +107,31 @@ public class ViewBoard extends javax.swing.JFrame
     private void initContainer() {
         int auxW = 900;
         int auxH = 20;
-
-        for (int rows = 0; rows < 9; ++rows) {
-            for (int col = 0; col < 5; ++col) {
+        int rows;
+        int col;
+        for (rows = 0; rows < 9; ++rows) {
+            for (col = 0; col < 5; ++col) {
                 cardPanelsContainer[rows][col] = new JPanel();//Crear Paneles
-                cardLabelsContainer[rows][col] = new JLabel();//Crear Labels
+                cardLabelsContainer[rows][col] = new LabelCell(rows, col);//Crear Labels
                 cardPanelsContainer[rows][col].add(cardLabelsContainer[rows][col]);//Añade el label dentro del panel creador
-                cardPanelsContainer[rows][col].setSize((widthContainer) / 9, (heigthContainer - 200) / 5);//Acomoda el tamaño del panel
+                cardPanelsContainer[rows][col].setSize((widthContainer) / 9, (heigthContainer) / 5);//Acomoda el tamaño del panel
                 cardPanelsContainer[rows][col].setLocation(auxW, auxH);//Acomoda al panel en la posición dada
-                auxH += (heigthContainer - 200) / 5;//Aumenta auxH para colocar el sigueinte label en la siguiente columna
+
+               // cardLabelsContainer[rows][col].setPosICol(auxH+10);
+                //cardLabelsContainer[rows][col].setPosIFilas(auxW+10);
+
+                int auxA = auxW + (widthContainer) / 9;
+                auxH += (heigthContainer) / 5;//Aumenta auxH para colocar el sigueinte label en la siguiente fila
+
+                //cardLabelsContainer[rows][col].setPosFCol(auxH - 10);
+                //cardLabelsContainer[rows][col].setPosFFilas(auxA-10);
+
                 this.add(cardPanelsContainer[rows][col]);//Añade el panel en el Frame
             }
-            auxW += (widthContainer) / 9; // Aumenta AuxW para colocarlo posicionarse en la nueva Fila
+
+            auxW += (widthContainer) / 9;
+
+// Aumenta AuxW para colocarlo posicionarse en la nueva Fila
             auxH = 20; // Reinicia auxH para colocar toda la nueva fila desde la primera columna
         }
         paintContainer();//Pinta todo el tablero
@@ -123,16 +145,13 @@ public class ViewBoard extends javax.swing.JFrame
         }
     }
 
-    private BoardLogic getControl() {
-        if (boardLogic == null) {
-            boardLogic = new BoardLogic(this);
-        }
-        return boardLogic;
-    }
-
     private void setLabels() {
         MouseListener ml = new MouseListener()
         {
+            LabelCell jc;
+            TransferHandler th;
+            LabelCell origin;
+
             @Override
             public void mouseClicked(MouseEvent e) {
             }
@@ -140,22 +159,34 @@ public class ViewBoard extends javax.swing.JFrame
             @Override
             public void mousePressed(MouseEvent e) {
                 int sourceX = e.getLocationOnScreen().x; // Almacena la posición inicial x
-                JComponent jc = (JComponent) e.getSource();
-                TransferHandler th = jc.getTransferHandler();
-
+                int sourceY = e.getLocationOnScreen().y;
+                jc = (LabelCell) e.getSource();
+                
+                System.out.println("Pos Inicial"+jc.getPosIFilas());
+                System.out.println("Pos Final"+jc.getPosFFilas());
+                
+                System.out.println(sourceX);
+                System.out.println(sourceY + "\n");
+                
+                //origin = getCell(jc.getPosICol(),jc.getPosFCol(),jc.getPosIFilas(),jc.getPosFFilas());
+                
+                th = jc.getTransferHandler();
                 // No realiza el drag en el mousePressed
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
                 int destinationX = e.getLocationOnScreen().x; // Obtiene la posición x donde se soltó el mouse
+                int destinationY = e.getLocationOnScreen().y;
 
+                System.out.println(destinationX);
+                System.out.println(destinationY + "\n");
                 // Verifica si la posición x de destino es menor o igual a 920
                 if (destinationX <= 920) {
-                    JComponent jc = (JComponent) e.getSource();
-                    TransferHandler th = jc.getTransferHandler();
+                    origin.setImage("C:\\\\Users\\\\Alejandro Penagos\\\\Desktop\\\\RummiQ\\\\RummiQ\\\\src\\\\Sprites\\\\Transparent.png");
                     th.exportAsDrag(jc, e, TransferHandler.COPY);
                 }
+
             }
 
             @Override
@@ -167,23 +198,24 @@ public class ViewBoard extends javax.swing.JFrame
             }
         };
 
-        for (JLabel[] row : labelsBoard) {
-            for (JLabel label : row) {
-                label.setIcon(new ImageIcon("C:\\\\Users\\\\Alejandro Penagos\\\\Desktop\\\\RummiQ\\\\RummiQ\\\\src\\\\Sprites\\\\Transparent.png"));
+        for (LabelCell[] row : labelsBoard) {
+            for (LabelCell label : row) {
+                label.setImage("C:\\\\Users\\\\Alejandro Penagos\\\\Desktop\\\\RummiQ\\\\RummiQ\\\\src\\\\Sprites\\\\Transparent.png");
                 label.addMouseListener(ml);
                 label.setTransferHandler(new TransferHandler("icon"));
             }
         }
 
-        for (JLabel[] row : cardLabelsContainer) {
-            for (JLabel label : row) {
-                label.setIcon(new ImageIcon("C:\\\\Users\\\\Alejandro Penagos\\\\Desktop\\\\RummiQ\\\\RummiQ\\\\src\\\\Sprites\\\\Corazones\\\\2Corazon.png"));
+        for (LabelCell[] row : cardLabelsContainer) {
+            for (LabelCell label : row) {
+                label.setImage("C:\\\\Users\\\\Alejandro Penagos\\\\Desktop\\\\RummiQ\\\\RummiQ\\\\src\\\\Sprites\\\\Corazones\\\\2Corazon.png");
                 label.addMouseListener(ml);
                 label.setTransferHandler(new TransferHandler("icon"));
             }
         }
     }
     
+  
 
     /**
      * This method is called from within the constructor to initialize the form.
